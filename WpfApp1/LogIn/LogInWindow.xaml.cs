a4432b2c33d2261d57c92ad999d36316cf5edfe7
Татьяна Воронцова <tdvorontsova@edu.hse.ru>
2018-11-15 21:34:36 +0300

    Add a window for LogIn

---
+++ WpfApp1/LogIn/LogInWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿//основное окно
+using Microsoft.Office.Interop.Excel;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Excel = Microsoft.Office.Interop.Excel;
+
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class LogInWindow : System.Windows.Window
+    {
+        private Range xlSheetRange;
+
+        // private Excel.Application xlApp;
+        // private Worksheet xlSheet;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+           
+        }
+        
+
+       
+
+    }
+    
+}
